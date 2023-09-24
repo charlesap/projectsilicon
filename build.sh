54e23efdec2f61c8e2ed0f149cd56cea9f1c5b3a
Charles Perkins <charlesap@gmail.com>
2023-09-24 14:55:29 -0700

    stub out native compiler until we have one

--- build.sh
+++ build.sh
@@ -13,14 +13,16 @@ echo
 echo "in interim"
 ../obnc/oac -s Out.Mod
 ../obnc/oac -s Files.Mod
-../obnc/oac -s Texts.Mod
 ../obnc/oac -s Fonts.Mod
+../obnc/oac -s Texts.Mod
 ../obnc/oac -s OAS.Mod
 ../obnc/oac -s OAB.Mod
 ../obnc/oac -s OAG.Mod
 ../obnc/oac -s OAL.Mod
 ../obnc/oac -s OAP.Mod
 ../obnc/oac -ms oac.Mod
+as exit.asm -o exit.o       #dummy compiler until interim one works
+ld exit.o -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib/ -lSystem -o oac
 popd 2>&1 >> /dev/null
 pushd . 2>&1 >> /dev/null
 cd native
@@ -29,8 +31,8 @@ echo
 echo "in native"
 ../interim/oac -s Out.Mod
 ../interim/oac -s Files.Mod
-../interim/oac -s Texts.Mod
 ../interim/oac -s Fonts.Mod
+../interim/oac -s Texts.Mod
 ../interim/oac -s OAS.Mod
 ../interim/oac -s OAB.Mod
 ../interim/oac -s OAG.Mod
